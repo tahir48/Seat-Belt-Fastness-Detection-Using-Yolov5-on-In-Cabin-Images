8637d0cc0d5044e642b15d0fe8a64b53a5bcbdfa
tahirbey48 <itkokten@gmail.com>
2022-07-28 21:37:16 +0300

    detectors added to runner script

--- runners.py
+++ runners.py
@@ -1,7 +1,10 @@
 import cv2
 from helpers import yolo_predictions
-
-
+from helpers import get_detections
+from helpers import non_maximum_supression
+from helpers2 import get_centroids
+from helpers2 import slope_detector
+from helpers2 import colinearity_detector
 
 
 
@@ -16,8 +19,18 @@ def seat_belt_detection(options):
     cv2.destroyAllWindows()
     return
 
-def fastness_detection(options):
-    return
+def fastness_detection(options, methods):
+    net = cv2.dnn.readNetFromONNX(options['model'])
+    net.setPreferableBackend(options['backend'])
+    net.setPreferableTarget(options['target'])
+    img = cv2.imread(options['image'])
+    input_image, detections = get_detections(img,net)
+    boxes_np, confidences_np, index = non_maximum_supression(input_image,detections)
+    centroids = get_centroids(img,boxes_np,index)
+    if methods["slope"] == True:
+        slope_detector(centroids)
+    if methods["colinearity"] == True:
+        colinearity_detector(centroids)
 
 
 
@@ -31,6 +44,11 @@ def run():
         "target": cv2.dnn.DNN_TARGET_CPU,
     }
 
+    methods = {
+        "slope" : True
+        "colinearity" : False
+    }
+
     seat_belt_detection(options)
-    fastness_detection(options)
+    fastness_detection(options, methods)
 
